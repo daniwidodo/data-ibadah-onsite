50bd7892df97bdfcb22751bb5f4111f847617b6c
bangtur_studio <dani.widodo87@gmail.com>
2021-11-13 13:19:37 +0800

    add logic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,7 +9,10 @@ import { Router } from '@angular/router';
 })
 export class HomePage {
 
-  endpoint = '';
+  endpoint = 'https://panelv1.gkisulsel.org/public/api/ibadahs';
+  ibadahName: any;
+  dataJemaat: any;
+  jumlahJemaat: any;
 
   constructor( public httpClient: HttpClient, public router: Router) {
 
@@ -19,13 +22,30 @@ export class HomePage {
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-    this.httpClient.get( this.endpoint + 'api/ibadahs')
-      .subscribe();
+    this.getAllIbadah();
 
   }
 
   goToDetails(ibadahId){
     this.router.navigate(['data-jemaat-details', ibadahId]);
+  }
+
+  getAllIbadah()
+  {
+    this.httpClient.get( this.endpoint )
+    .subscribe( data => {
+
+      const dataIbadah: any = data;
+      this.ibadahName = dataIbadah.data.namaIbadah;
+      this.jumlahJemaat = dataIbadah.data.jemaat.length;
+      this.dataJemaat = dataIbadah.data.jemaat;
+
+      ////
+      console.log(data);
+      console.log('ibadah name :', this.ibadahName);
+      console.log('jumlah Jemaat :', this.jumlahJemaat);
+      console.log('data jemaat :', this.dataJemaat);
+    });
   }
 
 }
